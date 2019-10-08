5dc89433d194fe32f75a9fd987bafd4ccb612a29
Antoine Berbineau <antoine.berbineau@gmail.com>
2019-10-08 23:25:51 +0200

    Add static initialiazer to the App

--- src/main/java/fr/berbineau/transaction/App.java
+++ src/main/java/fr/berbineau/transaction/App.java
@@ -2,6 +2,11 @@ package fr.berbineau.transaction;
 
 import static spark.Spark.get;
 
+import java.math.BigDecimal;
+
+import fr.berbineau.transaction.bank.Bank;
+import fr.berbineau.transaction.data.Account;
+
 /**
  * The basic app class
  * 
@@ -9,11 +14,21 @@ import static spark.Spark.get;
  *
  */
 public class App {
+    private static Bank bank = staticInitializer();
+
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello, world");
 
         get("/hello/:name", (req, res) -> {
             return "Hello, " + req.params(":name");
         });
+    }
+
+    public static Bank staticInitializer() {
+        Bank bank = Bank.getInstance();
+        for (long l = 1; l < 11; l++) {
+            bank.addAccount(new Account(l, BigDecimal.valueOf(100)));
+        }
+        return bank;
     }
 }
