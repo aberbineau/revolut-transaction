de6adfd00c013a2a154ac2078ed9e94f139ec978
Antoine Berbineau <antoine.berbineau@gmail.com>
2019-10-08 21:20:28 +0200

    Add javadoc

--- src/main/java/fr/berbineau/transaction/App.java
+++ src/main/java/fr/berbineau/transaction/App.java
@@ -2,6 +2,12 @@ package fr.berbineau.transaction;
 
 import static spark.Spark.get;
 
+/**
+ * The basic app class
+ * 
+ * @author aberbineau
+ *
+ */
 public class App {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello, world");
