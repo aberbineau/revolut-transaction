b483cf00c4015a6fee59d16ae8c71153986b6719
Antoine Berbineau <antoine.berbineau@gmail.com>
2019-10-08 21:22:19 +0200

    Add balance exception

---
+++ src/main/java/fr/berbineau/transaction/exceptions/BalanceException.java
@@ -0,0 +1,31 @@
+package fr.berbineau.transaction.exceptions;
+
+public class BalanceException extends Exception {
+
+    private static final long serialVersionUID = -8207796082878895896L;
+
+    public BalanceException() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public BalanceException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    public BalanceException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public BalanceException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public BalanceException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        // TODO Auto-generated constructor stub
+    }
+
+}
