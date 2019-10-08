ef5639d6276a460e491ec7d2d0773d6182b3ed66
Antoine Berbineau <antoine.berbineau@gmail.com>
2019-10-08 22:43:25 +0200

    Add UT for Transaction

---
+++ src/test/java/fr/berbineau/transaction/data/TransactionTest.java
@@ -0,0 +1,17 @@
+package fr.berbineau.transaction.data;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+
+public class TransactionTest {
+
+    @Test
+    public void constructorShouldThrowIllegalArgumentExceptionWhenAmountIsNegative() {
+        BigDecimal negativeAmount = BigDecimal.valueOf(-2.0);
+        assertThrows(IllegalArgumentException.class, () -> new Transaction(negativeAmount, 1L, 2L));
+    }
+
+}
